3e3244a83e305086d5ea8f68402a364ff116a374
mohsen <mim_z@hotmail.com>
2019-03-12 08:57:02 +0330

    add encryptAll method for encrypt string

--- EncryptString.java
+++ EncryptString.java
@@ -15,5 +15,30 @@ public class EncryptString {
     
     }// end method encrypt
 
+    // encryptAll method
+    public void encryptAll() {
+        getMessage();
+        getCode();
+
+        // assign length of string into a variable
+        int lengthOfString = inputString.length();
+        System.out.println("Your encrypted String with number " + inputInt + " is: ");
+
+        // encrypt each char in string except char space
+        for (int i = 0; i < lengthOfString; i++) {
+            int convertCharToInt = (int) encrypt(i);
+            if (convertCharToInt == 32) {
+                System.out.print(encrypt(i));
+            }// end of if
+            else {
+                convertCharToInt += inputInt;
+                char converIntTochar = (char) convertCharToInt;
+                System.out.print(converIntTochar);
+            }// end of else
+
+        }// end of for
+
+    }// end method encryptAll
+
         
 }// end class EncryptString
