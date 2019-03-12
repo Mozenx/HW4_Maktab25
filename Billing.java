ea65de7e107ea5f8d88d43d581d6a75770406585
mohsen <mim_z@hotmail.com>
2019-03-12 09:33:44 +0330

    add computeBill method with two parameter

--- Billing.java
+++ Billing.java
@@ -13,5 +13,14 @@ public class Billing {
         totalDue = price + tax;
         return totalDue;
     }
+
+    // computeBill method by two parameter
+    public static int computeBill(int price, int quantity){
+        //calculate tax
+        tax = price * quantity * 8 / 100;
+        // added tax
+        totalDue = price * quantity + tax;
+        return totalDue;
+    }
     
 }// end class Billing
