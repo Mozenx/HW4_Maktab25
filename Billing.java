4475cb78497def92318873be223763e458e106d3
mohsen <mim_z@hotmail.com>
2019-03-12 09:33:00 +0330

    add computeBill method with one parameter

--- Billing.java
+++ Billing.java
@@ -4,5 +4,14 @@ public class Billing {
     private static int tax;
     private static int off;
     private static int totalDue;
+
+    // computeBill method by one parameter
+    public static int computeBill(int price){
+        // calculate tax
+        tax = price * 8 / 100;
+        // added tax
+        totalDue = price + tax;
+        return totalDue;
+    }
     
 }// end class Billing
