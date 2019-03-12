ab1fe16fa9c5775a2a659be2fe2179f03b982abf
mohsen <mim_z@hotmail.com>
2019-03-12 09:17:53 +0330

    add displaySlutation method with one parameter add displaySlutation method with two parameter

--- FromLetterWriter.java
+++ FromLetterWriter.java
@@ -1,5 +1,15 @@
 package Q2;
 
 public class FromLetterWriter {
+
+    //displaySalutation method
+    public static void displaySalutation (String lastName){
+        System.out.println("Dear Mr. " + lastName);
+    }
+
+    // overloaded displaySalutation method
+    public static void displaySalutation (String firstName, String lastName){
+        System.out.println("Dear " + firstName + " " +lastName);
+    }
     
 }// end class FromLetterWriter
