03c3c50ca11c18bb5eb076f16555defb812f7647
mohsen <mim_z@hotmail.com>
2019-03-12 08:51:46 +0330

    add file. create EncrypString class. declare variables.

---
+++ EncryptString.java
@@ -0,0 +1,12 @@
+package Q1;
+
+import java.util.Scanner;
+
+public class EncryptString {
+    private String inputString;
+    private int inputInt;
+
+    Scanner input = new Scanner(System.in);
+
+    
+}// end class EncryptString
