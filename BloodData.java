61e36b99b73166bb5729a74f82cc797d94fd6b8b
mohsen <mim_z@hotmail.com>
2019-03-12 09:43:32 +0330

    add default constructor with add constructor with two parameter

--- BloodData.java
+++ BloodData.java
@@ -4,5 +4,17 @@ public class BloodData {
     private String bloodType;
     private char rhFactor;
 
+    //constructor
+    BloodData() {
+        this.bloodType = "O";
+        this.rhFactor = '+';
+    }
+
+    // constructor
+    BloodData(String bloodType, char rhFactor) {
+        this.bloodType = bloodType;
+        this.rhFactor = rhFactor;
+    }
+
     
 }// end class BloodData
