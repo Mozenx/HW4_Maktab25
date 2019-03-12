157fb3b6d98bbd2a640ce1bef51f9bd424b9d711
mohsen <mim_z@hotmail.com>
2019-03-12 09:54:12 +0330

    add default constructor add constructor provide value for each field

--- Patient.java
+++ Patient.java
@@ -5,4 +5,18 @@ public class Patient {
     private int age;
     private BloodData bloodData;
 
+    //default constructor
+    Patient() {
+        this.idNumber = 0;
+        this.age = 0;
+        this.bloodData = new BloodData();
+    }
+
+    // constructor provides values for each fields
+    Patient(int idNumber, int age, String bloodType, char rhFactor) {
+        this.idNumber = idNumber;
+        this.age = age;
+        this.bloodData = new BloodData(bloodType, rhFactor);
+    }
+
 }// end Patient class
